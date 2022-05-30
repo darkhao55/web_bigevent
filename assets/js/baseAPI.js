5ea26b14bf0a7055e73c2e16d04b2bcc4d222bf6
darkhao <850199417@qq.com>
2022-05-30 20:31:50 +0800

    完成了index页面

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,15 @@
 $.ajaxPrefilter((options) => {
     options.url = 'http://www.liulongbin.top:3007' + options.url
+        // 统一为有权限的接口，设置 headers 请求头
+    if (options.url.includes('/my/')) {
+        options.headers = {
+            Authorization: localStorage.getItem("token"),
+        };
+    }
+    options.complete = res => {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
 })
